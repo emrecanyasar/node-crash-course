ed4c14b800eadd2d6cb0eddd13b1e9eb21f1c0c2
Emre Can Yasar <emrecanyasar8@outlook.com>
2023-04-17 12:05:36 +0300

    ES6 Module System

---
+++ my-module.js
@@ -0,0 +1,21 @@
+const topla =(a,b)=> a+b;
+const cikar =(a,b)=> a-b;
+
+const hello = name => console.log(`hoşgeldin ${name}`);
+const text ="text";
+const user ={
+    name:"emre",
+    surname:"yasar"
+}
+
+const users = [{
+    name:"emre",
+    surname:"yasar"},
+    {
+        name:"emre",
+        surname:"yasar"
+    }
+];
+
+
+export {topla , hello, cikar,text,user,users};
